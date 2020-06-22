0412a68c50b272eac52258a2ff9636ef90b39eeb
CesarR22 <00015319@uca.edu.sv>
2020-06-22 16:40:44 -0600

    add score

--- Source Code/ARKANOID/Form1.Designer.cs
+++ Source Code/ARKANOID/Form1.Designer.cs
@@ -18,7 +18,7 @@
                 components.Dispose();
             }
 
-            base.Dispose(disposing);
+          
         }
 
         #region Windows Form Designer generated code
@@ -31,8 +31,9 @@
         {
             this.components = new System.ComponentModel.Container();
             this.picBall = new System.Windows.Forms.PictureBox();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
             this.picPaddle = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize) (this.picBall)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.picPaddle)).BeginInit();
             this.SuspendLayout();
@@ -41,39 +42,51 @@
             // 
             this.picBall.BackColor = System.Drawing.Color.Red;
             this.picBall.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.picBall.Location = new System.Drawing.Point(194, 137);
+            this.picBall.Location = new System.Drawing.Point(448, 453);
             this.picBall.Name = "picBall";
-            this.picBall.Size = new System.Drawing.Size(24, 25);
+            this.picBall.Size = new System.Drawing.Size(20, 21);
             this.picBall.TabIndex = 0;
             this.picBall.TabStop = false;
             // 
-            // timer1
+            // gameTimer
             // 
-            this.timer1.Enabled = true;
-            this.timer1.Interval = 10;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
             // 
             // picPaddle
             // 
             this.picPaddle.BackColor = System.Drawing.Color.Lime;
             this.picPaddle.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.picPaddle.Location = new System.Drawing.Point(194, 464);
+            this.picPaddle.Location = new System.Drawing.Point(434, 607);
             this.picPaddle.Name = "picPaddle";
-            this.picPaddle.Size = new System.Drawing.Size(199, 25);
+            this.picPaddle.Size = new System.Drawing.Size(137, 28);
             this.picPaddle.TabIndex = 1;
             this.picPaddle.TabStop = false;
+            // 
+            // txtScore
+            // 
+            this.txtScore.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.txtScore.Location = new System.Drawing.Point(14, 10);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(530, 23);
+            this.txtScore.TabIndex = 2;
+            this.txtScore.Text = "Score: 0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(990, 647);
+            this.Controls.Add(this.txtScore);
             this.Controls.Add(this.picPaddle);
             this.Controls.Add(this.picBall);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             ((System.ComponentModel.ISupportInitialize) (this.picBall)).EndInit();
             ((System.ComponentModel.ISupportInitialize) (this.picPaddle)).EndInit();
             this.ResumeLayout(false);
@@ -81,8 +94,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.PictureBox picBall;
         private System.Windows.Forms.PictureBox picPaddle;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer gameTimer;
     }
 }
--- Source Code/ARKANOID/Form1.cs
+++ Source Code/ARKANOID/Form1.cs
@@ -12,96 +12,206 @@ namespace Arkanoid
 {
   public partial class Form1 : Form
   {
-    public int vSpeed;
-    public int hSpeed;
-    public const int fila = 5;
-    public const int col = 6;
-    public PictureBox[,] blocks;
-    //CONSTRUCTOR
+    private bool goleft;
+    private bool goRight;
+    private bool isGameOver;
+
+    private int score;
+    private int ballx;
+    private int bally;
+    private int playerSpeed;
+    
+    Random rnd = new Random();
+    private PictureBox[] blockArray;
+    
+
     public Form1()
     {
-      vSpeed = 3;
-      hSpeed = 3;
-      setBlocks();
       InitializeComponent();
+      
+      placeBlocks();
     }
 
-    private void setBlocks()
+    private void setupGame()
     {
-      int blockHeight = 25;
-      int blockWidth = 100;
-      
-      blocks = new PictureBox[fila,col];
+      isGameOver = false;
+      score = 0;
+      ballx = 5;
+      bally = 5;
+      playerSpeed = 12;
+      txtScore.Text = "Score:" + score;
 
-      for (int x = 0; x < fila; x++)
+      picBall.Left = 448;
+      picBall.Top = 453;
+
+      picPaddle.Left =434 ;
+
+      gameTimer.Start();
+      //COLOREEEEEEEEEEEEEEEEEEES JBALVIN
+      foreach (Control x in this.Controls)
       {
-        for (int y = 0; y < col; y++)
+        if (x is PictureBox && (String) x.Tag == "blocks")
         {
-          blocks[x,y]= new PictureBox();
-
-          blocks[x, y].Width = blockWidth;
-          blocks[x, y].Height = blockHeight;
-          blocks[x,y].Top = blockHeight * x;
-          blocks[x, y].Left = blockWidth * y;
-          blocks[x, y].BackColor = Color.Navy;
-          blocks[x, y].BorderStyle = BorderStyle.Fixed3D;
-          
-          this.Controls.Add(blocks[x,y]);
-          
+          x.BackColor = Color.FromArgb(rnd.Next(256),rnd.Next(256),rnd.Next(256));
         }
+          
+      }
+    }
+
+    private void gameOver(string message)
+    {
+      isGameOver = true;
+      gameTimer.Stop();
+
+      txtScore.Text = "Score: " + score + "        " + message;
+    }
+
+    private void placeBlocks()
+    {
+      blockArray = new PictureBox[25];
+      int a = 0;
+      int top = 50;
+       int left = 100;
+
+       for (int i = 0; i < blockArray.Length; i++)
+       {
+         blockArray[i]=new PictureBox();
+         blockArray[i].Height = 25;
+         blockArray[i].Width = 100;
+         blockArray[i].Tag = "blocks";
+         blockArray[i].BackColor=Color.Peru;
+
+         if (a == 5)
+         {
+           top = top + 50;
+           left = 100;
+           a = 0;
+         }
+
+         if (a < 5)
+         {
+           a++;
+           blockArray[i].Left = left;
+           blockArray[i].Top = top;
+
+           this.Controls.Add(blockArray[i]);
+           left = left + 130;
+         }
+       }
+       setupGame();
+    }
+
+    private void removeBlocks()
+    {
+      foreach (PictureBox x in blockArray)
+      {
+        this.Controls.Remove(x);
+      }
+      
+    }
+    
+    
+    
+    //Controlesssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+    private void gameTimer_Tick(object sender, EventArgs e)
+    {
+      txtScore.Text = "Score: " + score;
+        
+      if (goleft == true && picPaddle.Left >5)
+      {
+        picPaddle.Left -= playerSpeed;
+      }
+      if (goRight == true && picPaddle.Left <725)
+      {
+        picPaddle.Left += playerSpeed;
+      }
+
+      picBall.Left += ballx;
+      picBall.Top += bally;
+
+      if (picBall.Left < 0 || picBall.Left> 826)
+      {
+        ballx = -ballx;
+      }
+
+      if (picBall.Top < 0)
+      {
+        bally = -bally;
+      }
+
+      if (picBall.Bounds.IntersectsWith(picPaddle.Bounds))
+      {
+        
+        bally = rnd.Next(5, 12) * -1;
+        if (ballx<0)
+        {
+          ballx = rnd.Next(5, 12) * -1;
+        }
+        else
+        {
+          ballx = rnd.Next(5, 12);
+        }
+      }
+      //Eliminacion de blocks
+
+      foreach (Control x in this.Controls)
+      {
+        if (x is PictureBox && (String) x.Tag == "blocks")
+        {
+          if (picBall.Bounds.IntersectsWith(x.Bounds))
+          {
+            score += 1;
+            bally = -bally;
+           
+            this.Controls.Remove(x);
+          }
+        }
+      }
+
+      if (score == 25)
+      {
+        gameOver("HAS SOBREVIVIDO AL CORONAVIRUS!!! PRESS ENTER ");
+      }
+
+      if (picBall.Top >550)
+      {
+        gameOver("MORISTE DE CORONAVIRUS!!! PRESS ENTER ");
+      }
+
+    
+    }
+    
+    private void Form1_KeyDown(object sender, KeyEventArgs e)
+    {
+      if (e.KeyCode == Keys.Left)
+      {
+        goleft = true;
+      }
+
+      if (e.KeyCode == Keys.Right)
+      {
+        goRight = true;
       }
     }
     
-    // CONTROL DE MOVIMIENTOS DE LA PELOTA
-    private void timer1_Tick(object sender, EventArgs e)
+    private void Form1_KeyUp(object sender, KeyEventArgs e)
     {
-      picBall.Top += vSpeed;
-      picBall.Left += hSpeed;
+      if (e.KeyCode == Keys.Left)
+      {
+        goleft = false;
+      }
 
-     if (picBall.Bottom > this.ClientSize.Height)
-     {
-       vSpeed = -vSpeed;
-     }
+      if (e.KeyCode == Keys.Right)
+      {
+        goRight = false;
+      }
 
-     if (picBall.Top < 0)
-     {
-       vSpeed = -vSpeed;
-     }
-     
-     if (picBall.Right > this.ClientSize.Width)
-     {
-       hSpeed = -hSpeed; 
-     }
-     if (picBall.Left < 0)
-     {
-       hSpeed = -hSpeed;
-     }
-     // CUANDO LA PELOTA CHOCA CON LA TABLA XD
-     if (picBall.Bounds.IntersectsWith(picPaddle.Bounds) == true )
-     {
-       vSpeed = -vSpeed;
-      // hSpeed = -hSpeed;
-     }
-     
-     //DESTRUCCION DE BLOQUES
-     for (int x = 0; x < fila; x++)
-     {
-       for (int y = 0; y < col; y++)
-       {
-         if (picBall.Bounds.IntersectsWith(blocks[x, y].Bounds) && blocks[x,y].Visible == true )
-         {
-           blocks[x, y].Visible = false;
-           vSpeed = -vSpeed;
-         }
-       }
-     }
-     
+      if (e.KeyCode == Keys.Enter && isGameOver == true)
+      {
+        removeBlocks();
+        placeBlocks();
+      }
     }
     
-   //CONTROL DE LA TABLE(PADDLE)
-    private void Form1_MouseMove(object sender, MouseEventArgs e)
-    {
-      picPaddle.Left = e.X - (picPaddle.Width / 2);
-    }
   }
 }
